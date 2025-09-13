57eed76ef604dfaa8cbdef91f467e2ec09fe9d7b
Jeremie moore <145256862+Jeremiegmoore@users.noreply.github.com>
2025-09-13 04:25:04 -0400

    Create full_recovery_runner1.js

---
+++ scripts/full_recovery_runner1.js
@@ -0,0 +1,47 @@
+{
+  "base_mainnet": {
+    "Multicall3": "0xcA11bde05977b3631167028862bE2a173976CA11",
+    "UniswapV3": {
+      "Permit2": "0x000000000022D473030F116dDEE9F6B43aC78BA3",
+      "UniversalRouter": "0x198EF79F1F515F02dFE9e3115eD9fC07183f02fC",
+      "CoreFactory": "0x33128a8fC17869897dcE68Ed026d694621f6FDfD",
+      "Multicall": "0x091e99cb1C49331a94dD62755D168E941AbD0693",
+      "ProxyAdmin": "0x3334d83e224aF5ef9C2E7DDA7c7C98Efd9621fA9",
+      "TickLens": "0x0CdeE061c75D43c82520eD998C23ac2991c9ac6d",
+      "NFTDescriptor": "0xF9d1077fd35670d4ACbD27af82652a8d84577d9F",
+      "PositionDescriptor": "0x4f225937EDc33EFD6109c4ceF7b560B2D6401009",
+      "DescriptorProxy": "0x4615C383F85D0a2BbED973d83ccecf5CB7121463",
+      "PositionManager": "0x03a520b32C04BF3bEEf7BEb72E919cf822Ed34f1",
+      "Migrator": "0x23cF10b1ee3AdfCA73B0eF17C07F7577e7ACd2d7",
+      "Staker": "0x42bE4D6527829FeFA1493e1fb9F3676d2425C3C1",
+      "QuoterV2": "0x3d4e44Eb1374240CE5F1B871ab261CD16335B76a",
+      "SwapRouter": "0x2626664c2603336E57B271c5C0b26F421741e481"
+    },
+    "UniswapV2": {
+      "Factory": "0x8909Dc15e40173Ff4699343b6eB8132c65e18eC6",
+      "Router": "0x4752ba5dbc23f44d87826276bf6fd6b1c372ad24"
+    }
+  },
+  "base_sepolia": {
+    "Multicall3": "0xcA11bde05977b3631167028862bE2a173976CA11",
+    "UniswapV3": {
+      "Permit2": "0x000000000022d473030f116ddee9f6b43ac78ba3",
+      "UniversalRouter": "0x050E797f3625EC8785265e1d9BDd4799b97528A1",
+      "CoreFactory": "0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24",
+      "Multicall": "0xd867e273eAbD6c853fCd0Ca0bFB6a3aE6491d2C1",
+      "ProxyAdmin": "0xD7303474Baca835743B54D73799688990f24a79D",
+      "TickLens": "0xedf6066a2b290C185783862C7F4776A2C8077AD1",
+      "NFTDescriptor": "0x4e0caFF1Df1cCd7CF782FDdeD77f020699B57f1a",
+      "PositionDescriptor": "0xd7c6e867591608D32Fe476d0DbDc95d0cf584c8F",
+      "PositionManager": "0x27F971cb582BF9E50F397e4d29a5C7A34f11faA2",
+      "Migrator": "0xCbf8b7f80800bd4888Fbc7bf1713B80FE4E23E10",
+      "Staker": "0x62725F55f50bdE240aCa3e740D47298CAc8d57D5",
+      "QuoterV2": "0xC5290058841028F1614F3A6F0F5816cAd0df5E27",
+      "SwapRouter": "0x94cC0AaC535CCDB3C01d6787D6413C739ae12bc4"
+    },
+    "UniswapV2": {
+      "Factory": "0x7Ae58f10f7849cA6F5fB71b7f45CB416c9204b1e",
+      "Router": "0x1689E7B1F10000AE47eBfE339a4f69dECd19F602"
+    }
+  }
+    }
